7c0e223464d3b1ca805ad3dca504983a263d4cdb
bikash-dash <vikashdash004@gmail.com>
2021-07-02 21:23:20 +0530

    updated the password

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const { graphqlHTTP} = require('express-graphql');
 const Schema = require('./schema/schema')
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://bikash:dvPHT1xEqBOiZF8F@cluster0.j6es8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+mongoose.connection.once("open", () => {
+    console.log('---->connection to database opened');
+})
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
@@ -8,5 +19,5 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 app.listen(4001,()=>{
-    console.log('Listening to http://localhost:000');
+    console.log('Listening to http://localhost:4001');
 })
